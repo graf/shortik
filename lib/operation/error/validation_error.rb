5b89fae47c3e0436886f28813bdc52bc65c37fc5
Pavel Abolmasov <graf@abolmasov.pro>
2020-10-25 19:18:29 +0400

    Fix Operation::Error::ValidationError

--- lib/operation/error/validation_error.rb
+++ lib/operation/error/validation_error.rb
@@ -2,12 +2,13 @@
 
 module Operation
   module Error
-    class ValidationError
+    class ValidationError < BaseError
       attr_reader :model
 
       delegate :errors, to: :model
 
       def initialize(model)
+        super('Validation failed')
         @model = model
       end
     end
